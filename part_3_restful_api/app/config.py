78d5a057e49537f59837acfcd8fbc284d87baad9
algonacci <86970816+algonacci@users.noreply.github.com>
2021-08-25 22:21:20 +0700

    Add files via upload

---
+++ part_3_restful_api/app/config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    MONGODB_DB_NAME = 'flask_mentoring'
+    MONGODB_DB_HOST = 'localhost'
+    MONGODB_DB_PORT = 27017
+    MONGODB_DB_USERNAME = ''
+    MONGODB_DB_PASSWORD = ''
